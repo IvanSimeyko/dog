c91fcc61e298470facb040a2c73bae47eac070b5
unknown <rus@gmail.com>
2015-10-04 16:06:37 +0300

    Add index.html

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,28 @@
-console.log('This would be the main JS file.');
+// $ document ready
+$(function () {
+
+	// находим форму
+	var $searhForm = $('#searh-form');
+	// находим input
+	var $query = $('#query');
+
+	// создаем функцию коллбек для submit формы	
+	var onSubmitHandler = function (event) {
+		// this - это объект элемента на котором произошло событие
+		var inputValue = $query.val();
+		console.log('onSubmitHandler', inputValue);
+
+		var error, success;
+		
+		error = function(data) {
+			console.log(data);
+		}
+		success = error;
+
+		nestoriaApi.getLocations(inputValue, success, error);
+
+		event.preventDefault();
+	};
+
+	$searhForm.on('submit', onSubmitHandler);
+});
