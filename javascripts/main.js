b63bdc79ecdbd1d319b7dc45f201a933667e5a1c
IvanSimeyko <iv.simeyko@gmail.com>
2015-10-10 22:12:39 +0300

    added func

--- javascripts/main.js
+++ javascripts/main.js
@@ -88,7 +88,23 @@ $(function () {
 
 	var showLocationsList = function(){
 		console.info('showLocationsList');
+	var showLocationsList = function (response){
+		var elements = response.locations;
+		var fragment = document.createDocumentFragment();
+		var existingUl = document.getElementById('result_2');
+		function someFunc (element) {
+			var li = document.createElement('li');
+			var p = document.createElement('p');
+			p.innerHTML = element.long_title;
+			li.appendChild(p);
+
+			fragment.appendChild(li);
+		}
+		elements.forEach(someFunc);
+		existingUl.innerHTML = '<h2> Возможно вы имели ввиду: </h2>';
+		existingUl.appendChild(fragment);
 	};
+    };
 
 	var showErrorList = function(){
 		console.error('showErrorList');
