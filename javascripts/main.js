b8cb66fe5a3a2c678000bc8f81d38c00e824880b
IvanSimeyko <iv.simeyko@gmail.com>
2015-10-18 12:32:07 +0300

    modify showDataFromLocalStorage

--- javascripts/main.js
+++ javascripts/main.js
@@ -39,17 +39,17 @@ $(function () {
     //show data from localStorage
     var showDataFromLocalStorage = function() {
         console.log('showDataFromLocalStorage');
-        var elements = JSON.parse( localStorage.getItem("successLocations") );
-        console.log (elements);
+        var array = JSON.parse( localStorage.getItem("successLocations") );
+        console.log (array);
         var fragment = document.createDocumentFragment();
         var existingUl = document.getElementById('result_2');
 
-        if (elements) {
-        for (var i = 0; i < elements.length; i++) {
+        if (array) {
+            function someFunc (element, index, array) {
             var li = document.createElement('li');
             var a = document.createElement ('a');
-            a.href  =  '#' + elements[i];
-            a.innerHTML = elements[i];
+            a.href  =  '#' + element;
+            a.innerHTML = element;
 
             a.addEventListener("click", function() {
                 console.log( 'was click the link' );
@@ -67,71 +67,53 @@ $(function () {
             fragment.appendChild(li);
         }
         }
+        array.forEach(someFunc);
+        existingUl.innerHTML='';
         existingUl.appendChild(fragment);
+
     };
 
     showDataFromLocalStorage();
 
+    // response_code 100 or 101 or 110
+    // native js
 	var showBuildingsList = function(response){
-		console.info('showBuildingsList', response);
-
-        var html = '';
-        var existingUl = document.getElementById('result_2');
-        var template = '<li><img src="## img_url ##"><h2><% title %></h2><p>{{ summary }}</p></li>';
-
-        function someFunc (element) {
-            var newLi = template;
-
-            newLi = newLi.replace('## img_url ##', element.img_url);
-            newLi = newLi.replace('<% title %>', element.title);
-            newLi = newLi.replace('{{ summary }}', element.summary);
-
-            html += newLi;
-          }
-          response.listings.forEach(someFunc);
-
-          // добавление fragment в existingUl
-          existingUl.innerHTML = html;
-
-         //
-         //localStorage.setItem("successLocations", text);
-         //localStorage.getItem("successLocations");
-         //JSON.stringify
-         //JSON.parse
-    };
-		// еще пару вариантов решения данной фундкции
-		// jQuery
-		/*var $listingsList = $('#listings-list');
-		var htmlList = $listingsList.html();
-		var listingTemplate = _.template( htmlList );
-		var newHtml = listingTemplate({
-			items: response.listings
-		});
-
-		$result.html( newHtml );*/
-
-
+        console.log ( 'run showBuildingsList');
         // native js
-        /*var listings = response.listings;
-        console.log ('listings= ', listings);
-        //var body = document.body;
+        var elements  = response.listings;
+        console.log ('listings= ', elements);
         var fragment = document.createDocumentFragment();
         var existingUl = document.getElementById('result_2');
 
         function someFunc (element) {
             var li = document.createElement('li');
             var img = document.createElement('img');
-            li.innerHTML = element.title;
+            var h2 = document.createElement('h2');
+			var p1 = document.createElement('p');
+			var p2 = document.createElement('p');
+
+            img.src = element.img_url;
+			li.appendChild(img);
+
+			h2.innerHTML = element.price_formatted;
+			li.appendChild(h2);
+
+			p1.innerHTML = element.title;
+			li.appendChild(p1);
+
+			p2.innerHTML = element.summary;
+			li.appendChild(p2);
+
             fragment.appendChild(li);
             //console.log ('fragment= ', fragment)
-        };
-        listings.forEach(someFunc);
+        }
+        elements.forEach(someFunc);
 
         existingUl.innerHTML='';
-        existingUl.appendChild(fragment)*/
+        existingUl.appendChild(fragment)
+    };
 
     // response_code 200 or 202
-    /*
     // native js
 	    var showLocationsList = function (response){
 		console.log ( 'run showLocationsList');
@@ -165,33 +147,12 @@ $(function () {
         }
 
         elements.forEach(someFunc);
-        //existingUl.innerHTML='';
+        existingUl.innerHTML='';
         existingUl.appendChild(fragment);
     };
-    */
-
-    // response_code 200 or 202
-    // underscore.js
-    var showLocationsList = function(response){
-		// response.locations
-		// $result
-		console.info('showLocationsList', response.locations);
-        // получили html-фрагмент в виде строки
-		var htmlTemplate = $('#locations-list').html();
-        console.log( 'htmlTemplate= ', htmlTemplate );
-
-		var funcTemplate = _.template(htmlTemplate);
-
-		var newHtmlTemplate = funcTemplate({
-			locations: response.locations,
-			first: '<li>1</li>'
-		});
-
-		$result.html( newHtmlTemplate );
-	};
 
     // response_code 201
-   /* // native js
+    // native js
 	var showErrorList = function(response){
         console.log('showErrorList');
         console.log( response );
@@ -204,31 +165,11 @@ $(function () {
         p.innerHTML = 'your request did not produce results. The response from the server - ' + answer;
         li.appendChild(p);
         fragment.appendChild(li);
-        //existingUl.innerHTML='';
+        existingUl.innerHTML='';
         existingUl.appendChild(fragment);
-	};*/
-
-    // response_code 201
-    // underscore.js
-    var showErrorList = function(response){
-		// response.locations
-		// $result
-		console.info('showErrorList', response.application_response_text);
-        // получили html-фрагмент в виде строки
-		var htmlTemplate = $('#error-list').html();
-        console.log( 'htmlTemplate= ', htmlTemplate );
-
-		var funcTemplate = _.template(htmlTemplate);
-
-		var newHtmlTemplate = funcTemplate({
-			locations: response.application_response_text,
-			first: '<li>1</li>'
-		});
-
-		$result.html( newHtmlTemplate );
 	};
 
-	var onChangeQueryText = function (event, text) {
+   	var onChangeQueryText = function (event, text) {
 		console.log('querytext:change');
 		var callbackSuccess = function(data) {
 			console.info(data);
@@ -275,3 +216,87 @@ $(function () {
     $query.on('querytext:change', onChangeQueryText);
 
 });
+
+/*
+// Другие варианты напсания функций
+// вариант showBuildingsList с помощью шаблонизатора
+var showBuildingsList = function(response){
+		console.info('showBuildingsList', response);
+
+        var html = '';
+        var existingUl = document.getElementById('result_2');
+        var template = '<li><img src="## img_url ##"><h2><% title %></h2><p>{{ summary }}</p></li>';
+
+        function someFunc (element) {
+            var newLi = template;
+
+            newLi = newLi.replace('## img_url ##', element.img_url);
+            newLi = newLi.replace('<% title %>', element.title);
+            newLi = newLi.replace('{{ summary }}', element.summary);
+
+            html += newLi;
+          }
+          response.listings.forEach(someFunc);
+
+          // добавление fragment в existingUl
+          existingUl.innerHTML = html;
+
+         //
+         //localStorage.setItem("successLocations", text);
+         //localStorage.getItem("successLocations");
+         //JSON.stringify
+         //JSON.parse
+    };
+
+// вариант showBuildingsList с помощью jQuery
+var showBuildingsList = function(response){
+		var $listingsList = $('#listings-list');
+		var htmlList = $listingsList.html();
+		var listingTemplate = _.template( htmlList );
+		var newHtml = listingTemplate({
+			items: response.listings
+		});
+
+		$result.html( newHtml );
+};
+
+ // вариант функции response_code 200 or 202
+    // underscore.js
+    var showLocationsList = function(response){
+		// response.locations
+		// $result
+		console.info('showLocationsList', response.locations);
+        // получили html-фрагмент в виде строки
+		var htmlTemplate = $('#locations-list').html();
+        console.log( 'htmlTemplate= ', htmlTemplate );
+
+		var funcTemplate = _.template(htmlTemplate);
+
+		var newHtmlTemplate = funcTemplate({
+			locations: response.locations,
+			first: '<li>1</li>'
+		});
+
+		$result.html( newHtmlTemplate );
+	};
+
+ // response_code 201
+    // underscore.js
+    var showErrorList = function(response){
+		// response.locations
+		// $result
+		console.info('showErrorList', response.application_response_text);
+        // получили html-фрагмент в виде строки
+		var htmlTemplate = $('#error-list').html();
+        console.log( 'htmlTemplate= ', htmlTemplate );
+
+		var funcTemplate = _.template(htmlTemplate);
+
+		var newHtmlTemplate = funcTemplate({
+			locations: response.application_response_text,
+			first: '<li>1</li>'
+		});
+
+		$result.html( newHtmlTemplate );
+	};
+	*/
